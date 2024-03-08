695bfbca9e9d282fb81411e523699230aeeeb765
Sergey Leshkevich <s.leshkevichemail@gmail.com>
2024-03-08 06:15:31 +0300

    refactor

--- src/main/java/ru/clevertec/cache/config/AppConfig.java
+++ src/main/java/ru/clevertec/cache/config/AppConfig.java
@@ -1,12 +1,14 @@
 package ru.clevertec.cache.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.clevertec.cache.entity.CacheFactory;
 import ru.clevertec.cache.entity.impl.CacheFactoryImpl;
 
 @Configuration
+@EnableConfigurationProperties(AppConfig.class)
 public class AppConfig {
 
     @Bean
